8bd0fbf198ab9d99de8462076270406d17d0bb62
Nattarika-Cha <s5902041620113@email.kmutnb.ac.th>
2021-02-03 02:53:38 +0700

    Header

---
+++ src/template/Header2.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Container, Image } from 'react-bootstrap';
+import { Row } from 'antd';
+import { withRouter } from "react-router-dom";
+import '../css/Header.css';
+import banner from '../img/banner.png';
+
+export default withRouter(class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        
+        }
+    }
+
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Image src={banner} fluid></Image>
+                </Row>
+            </Container>
+        )
+    }
+})

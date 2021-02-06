3078f87157b14c8d03ffa768daf5cc34f5ed8e35
Nattarika-Cha <s5902041620113@email.kmutnb.ac.th>
2021-02-06 13:53:03 +0700

    changepass & Forgetpass

--- src/template/Header.js
+++ src/template/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
 import { Row, Col, Button, Modal, Form, Input } from 'antd';
 import { withRouter } from "react-router-dom";
 import { NavLink } from 'react-router-dom';
 import '../css/Header.css';
 import { QuestionCircleOutlined } from '@ant-design/icons';
-
+import { FaCheckCircle } from "react-icons/fa";
+import letter from "../img/letter.png"
 export default withRouter(class Header extends Component {
     constructor(props) {
         super(props);
@@ -13,10 +14,15 @@ export default withRouter(class Header extends Component {
             token: "",
             isModalVisible: false,
             isModalForgetPass: false,
+            isModalchangePass: false,
+            isModalCPSuccess: false,
+            isModalConfirmMail: false,
         }
 
         this.showModal = this.showModal.bind(this);
         this.showForgetPass = this.showForgetPass.bind(this);
+        this.showConfirmMail = this.showConfirmMail.bind(this);
+        this.showchangePass = this.showchangePass.bind(this);
     }
 
     showModal() {
@@ -25,6 +31,18 @@ export default withRouter(class Header extends Component {
 
     showForgetPass(){
         this.setState({isModalForgetPass: true, isModalVisible: false});
+    }
+
+    showConfirmMail(){
+        this.setState({isModalConfirmMail: true, isModalForgetPass: false, isModalVisible: false});
+    }
+
+    showchangePass(){
+        this.setState({ isModalchangePass: true });
+    }
+
+    showCPSuccess(){
+        this.setState({ isModalCPSuccess: true, isModalchangePass: false });
     }
 
     render() {
@@ -38,6 +56,7 @@ export default withRouter(class Header extends Component {
                             <Col xs={24} md={24} xl={24} id="btn-header">
                                 <Button type="primary" id="btn-sty" onClick={() => this.showModal()}>เข้าสู่ระบบ</Button>
                                 <NavLink to="/Register"><Button type="primary" id="btn-sty1">สมัครสมาชิก</Button></NavLink>
+                                <Button type="primary" id="btn-sty" onClick={() => this.showchangePass()}>เปลี่ยนรหัสผ่าน</Button>
                             </Col>
                         </Row>
                     </Col>
@@ -128,14 +147,102 @@ export default withRouter(class Header extends Component {
                                             </Form.Item>
                                         </Col>
                                     <Col id="row-btnForgetPass">
-                                        <Button id="btn-fotgetPass">ตรวจสอบ</Button>
+                                        <Button id="btn-fotgetPass" onClick={() => this.showConfirmMail()}>ตรวจสอบ</Button>
                                     </Col>
                                 </Col>
                                 <Col xs={3} md={3} xl={3}></Col>
                             </Row>
-                            <Col xs={24} md={24} xl={24} id="ft-footer"><QuestionCircleOutlined style={{fontSize: "10px", color: "#C4C4C4", display: "flex", alignItems: "center", marginRight: "0.5%"}}/>หากคุณลืมอีเมลที่ใช้ในการลงทะเบียนกรุณาติดต่อที่ 082-222-2232 หรือทางอีเมลที่ Dairc.kmutnb@gmail.com</Col>
+                            <Col xs={24} md={24} xl={24} id="ft-footer"><QuestionCircleOutlined style={{fontSize: "10px", display: "flex", alignItems: "center", marginRight: "0.5%"}}/>หากคุณลืมอีเมลที่ใช้ในการลงทะเบียนกรุณาติดต่อที่ 082-222-2232 หรือทางอีเมลที่ Dairc.kmutnb@gmail.com</Col>
                         </Form>
                 </Modal>
+
+                <Modal
+                    title={null}
+                    footer={null}
+                    visible={this.state.isModalchangePass}
+                    width={450}>
+                        <Form>
+                            <Row id="row-CP">
+                                <Col xs={4} md={4} xl={4}></Col>
+                                <Col xs={16} md={16} xl={16}>
+                                    <Col xs={24} md={24} xl={24} id="CPHeader">สร้างรหัสผ่านใหม่</Col>
+                                    <Row  id="CPDescript">รหัสผ่าน</Row>
+                                        <Col xs={24} md={24} xl={24}>
+                                            <Form.Item
+                                                name="newPassword"
+                                                rules={[
+                                                {
+                                                    required: true,
+                                                    message: 'กรุณากรอกรหัสผ่าน!',
+                                                },
+                                                ]}>
+                                                <Input.Password  placeholder="รหัสผ่าน" id="CPInput" />
+                                            </Form.Item>   
+                                        </Col>
+                                        <Row  id="CPDescript1">รหัสผ่านใหม่</Row>
+                                        <Col xs={24} md={24} xl={24}>
+                                            <Form.Item
+                                                name="newPassword1"
+                                                rules={[
+                                                {
+                                                    required: true,
+                                                    message: 'กรุณากรอกรหัสผ่าน!',
+                                                },
+                                                ]}>
+                                                <Input.Password  placeholder="รหัสผ่าน" id="CPInput" />
+                                            </Form.Item>   
+                                        </Col>
+                                        <Row>
+                                            <Button id="btn-CP" onClick={() => this.showCPSuccess()}>เปลี่ยนรหัสผ่าน</Button>
+                                        </Row>
+                                </Col>
+                                <Col xs={4} md={4} xl={4}></Col>
+                            </Row>
+                        </Form>
+                </Modal>
+
+                <Modal
+                    title={null}
+                    footer={null}
+                    visible={this.state.isModalCPSuccess}
+                    width={450}>
+                        <Row id="Modal-Chanepass">
+                            <Col xs={4} md={4} xl={4}></Col>
+                            <Col xs={16} md={16} xl={16}>
+                                <Row id="Row-Modal">
+                                    <FaCheckCircle style={{fontSize: "70px", color: "#8DC53E"}}/>
+                                </Row>
+                                <Row id="Modal-CP">เปลี่ยนรหัสผ่านสำเร็จ</Row>
+                                <Row id="Modal-CP1">กรุณาเข้าสู่ระบบอีกครั้ง</Row>
+                                <Row id="Row-CP">
+                                    <Button id="btn-ModalCP1">เข้าสู่ระบบ</Button>
+                                </Row>
+                            </Col>
+                            <Col xs={4} md={4} xl={4}></Col>
+                        </Row>
+                    </Modal>
+
+                    <Modal
+                        title={null}
+                        footer={null}
+                        visible={this.state.isModalConfirmMail}
+                        width={450}>
+                        <Row id="Modal-Chanepass">
+                            <Col xs={4} md={4} xl={4}></Col>
+                            <Col xs={16} md={16} xl={16}>
+                                <Row id="Row-Modal">
+                                    <Image src={letter} fluid/>
+                                </Row>
+                                <Row id="Modal-cfMail">ตรวจสอบอีเมลล์ของคุณ</Row>
+                                <Row id="Modal-cfMail1">เราได้ส่งคำแนะนำในการกู้คืนรหัสผ่านไปยังอีเมลล์ของคุณ</Row>
+                                <Row id="Row-CP">
+                                    <Button id="btn-ModalCf1">เสร็จสิ้น</Button>
+                                </Row>
+                            </Col>
+                            <Col xs={4} md={4} xl={4}></Col>
+                            <Col xs={24} md={24} xl={24}id="ft-footer"><QuestionCircleOutlined style={{fontSize: "10px", display: "flex", alignItems: "center", marginRight: "0.5%"}}/>หากไม่ได้รับอีเมลล์? โปรดตรวจสอบอีเมลล์ของคุณหรือลองใช้ที่อยู่อีเมลล์อื่น</Col>
+                        </Row>
+                    </Modal>
             </Container>
         )
     }

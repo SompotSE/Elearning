eb1c3d8b9b36a7f606c9784963a30c951b1910de
SompotSE <SompotSE>
2021-02-14 17:02:24 +0700

    Admin Top Score

--- src/content/Admin/AdminTopScore.js
+++ src/content/Admin/AdminTopScore.js
@@ -1,12 +1,70 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
-import { Row, Col, Progress  } from 'antd';
+import { Row, Col, Progress } from 'antd';
 import '../../css/AdminTopScore.css';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+import swal from 'sweetalert';
+import { config } from '../../config/config';
+
+const cookies = new Cookies();
+
+const ip = config.ipServer;
+
+const CourseCode1 = "COURSE1001";
+const CourseCode2 = "COURSE1002";
+const CourseCode3 = "COURSE1003";
+const CourseCode4 = "COURSE1004";
+const CourseCode5 = "COURSE1005";
 
 export default class AdminTopScore extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            token: "",
+            email: "",
+            header: [],
+            topscore: [],
+            topscore1: [],
+            topscore2: [],
+            topscore3: [],
+            topscore4: [],
+            topscore5: []
+        }
+    }
+
+    componentWillMount() {
+        this.setState({
+            token: cookies.get('token_user', { path: '/' }),
+            email: cookies.get('email', { path: '/' }),
+            header: {
+                token: cookies.get('token_user', { path: '/' }),
+                key: cookies.get('email', { path: '/' })
+            }
+        });
+    }
+
+    async componentDidMount() {
+        var url_exam_topscore = ip + "/Examination/ExamPost/TopScore";
+        const exam_topscore = await (await axios.get(url_exam_topscore, { headers: this.state.header })).data;
+        if (!exam_topscore?.status) {
+            swal("Error!", "เกิดข้อผิดพลาดในการเข้าสู่ระบบ \n กรุณาเข้าสู่ระบบใหม่", "error").then((value) => {
+                this.setState({
+                    token: cookies.remove('token_user', { path: '/' }),
+                    user: cookies.remove('user', { path: '/' }),
+                    email: cookies.remove('email', { path: '/' })
+                });
+                window.location.replace('/Login', false);
+            });
+        } else {
+            this.setState({
+                topscore: exam_topscore.data,
+                topscore1: exam_topscore.data?.filter((item) => item.courseCode === CourseCode1),
+                topscore2: exam_topscore.data?.filter((item) => item.courseCode === CourseCode2),
+                topscore3: exam_topscore.data?.filter((item) => item.courseCode === CourseCode3),
+                topscore4: exam_topscore.data?.filter((item) => item.courseCode === CourseCode4),
+                topscore5: exam_topscore.data?.filter((item) => item.courseCode === CourseCode5)
+            });
         }
     }
 
@@ -24,37 +82,37 @@ export default class AdminTopScore extends Component {
 
                 <Row id="row-table-AdminTopScore1">
                     <Col md={5} xl={5} id="TopScore">หลักสูตรที่1</Col>
-                    <Col md={5} xl={4} id="TopScore"><Progress type="circle" percent={75} width={50} strokeWidth={13}/></Col>
-                    <Col md={6} xl={7} id="TopScore1">ชื่อ - นามสกุล</Col>
-                    <Col md={7} xl={7} id="TopScore1">ชื่อบริษัท</Col>
+                    <Col md={5} xl={4} id="TopScore"><Progress type="circle" percent={this.state.topscore1[0]?.percenScore} width={50} strokeWidth={13} strokeColor={(parseInt(this.state.topscore1[0]?.percenScore)) >= 80 ? "#006633" : "#CC0000"}/></Col>
+                    <Col md={6} xl={7} id="TopScore1">{this.state.topscore1[0]?.name}</Col>
+                    <Col md={7} xl={7} id="TopScore1">{this.state.topscore1[0]?.nameCompany}</Col>
                 </Row>
 
                 <Row id="row-table-AdminTopScore1">
                     <Col md={5} xl={5} id="TopScore">หลักสูตรที่2</Col>
-                    <Col md={5} xl={4} id="TopScore"><Progress type="circle" percent={75} width={50} strokeWidth={13}/></Col>
-                    <Col md={6} xl={7} id="TopScore1">ชื่อ - นามสกุล</Col>
-                    <Col md={7} xl={7} id="TopScore1">ชื่อบริษัท</Col>
+                    <Col md={5} xl={4} id="TopScore"><Progress type="circle" percent={this.state.topscore2[0]?.percenScore} width={50} strokeWidth={13} strokeColor={(parseInt(this.state.topscore2[0]?.percenScore)) >= 80 ? "#006633" : "#CC0000"}/></Col>
+                    <Col md={6} xl={7} id="TopScore1">{this.state.topscore2[0]?.name}</Col>
+                    <Col md={7} xl={7} id="TopScore1">{this.state.topscore2[0]?.nameCompany}</Col>
                 </Row>
 
                 <Row id="row-table-AdminTopScore1">
                     <Col md={5} xl={5} id="TopScore">หลักสูตรที่3</Col>
-                    <Col md={5} xl={4} id="TopScore"><Progress type="circle" percent={75} width={50} strokeWidth={13}/></Col>
-                    <Col md={6} xl={7} id="TopScore1">ชื่อ - นามสกุล</Col>
-                    <Col md={7} xl={7} id="TopScore1">ชื่อบริษัท</Col>
+                    <Col md={5} xl={4} id="TopScore"><Progress type="circle" percent={this.state.topscore3[0]?.percenScore} width={50} strokeWidth={13} strokeColor={(parseInt(this.state.topscore3[0]?.percenScore)) >= 80 ? "#006633" : "#CC0000"} /></Col>
+                    <Col md={6} xl={7} id="TopScore1">{this.state.topscore3[0]?.name}</Col>
+                    <Col md={7} xl={7} id="TopScore1">{this.state.topscore3[0]?.nameCompany}</Col>
                 </Row>
 
                 <Row id="row-table-AdminTopScore1">
                     <Col md={5} xl={5} id="TopScore">หลักสูตรที่4</Col>
-                    <Col md={5} xl={4} id="TopScore"><Progress type="circle" percent={75} width={50} strokeWidth={13}/></Col>
-                    <Col md={6} xl={7} id="TopScore1">ชื่อ - นามสกุล</Col>
-                    <Col md={7} xl={7} id="TopScore1">ชื่อบริษัท</Col>
+                    <Col md={5} xl={4} id="TopScore"><Progress type="circle" percent={this.state.topscore4[0]?.percenScore} width={50} strokeWidth={13} strokeColor={(parseInt(this.state.topscore4[0]?.percenScore)) >= 80 ? "#006633" : "#CC0000"} /></Col>
+                    <Col md={6} xl={7} id="TopScore1">{this.state.topscore4[0]?.name}</Col>
+                    <Col md={7} xl={7} id="TopScore1">{this.state.topscore4[0]?.nameCompany}</Col>
                 </Row>
 
                 <Row id="row-table-AdminTopScore1">
                     <Col md={5} xl={5} id="TopScore">หลักสูตรที่5</Col>
-                    <Col md={5} xl={4} id="TopScore"><Progress type="circle" percent={75} width={50} strokeWidth={13}/></Col>
-                    <Col md={6} xl={7} id="TopScore1">ชื่อ - นามสกุล</Col>
-                    <Col md={7} xl={7} id="TopScore1">ชื่อบริษัท</Col>
+                    <Col md={5} xl={4} id="TopScore"><Progress type="circle" percent={this.state.topscore5[0]?.percenScore} width={50} strokeWidth={13} strokeColor={(parseInt(this.state.topscore5[0]?.percenScore)) >= 80 ? "#006633" : "#CC0000"} /></Col>
+                    <Col md={6} xl={7} id="TopScore1">{this.state.topscore5[0]?.name}</Col>
+                    <Col md={7} xl={7} id="TopScore1">{this.state.topscore5[0]?.nameCompany}</Col>
                 </Row>
             </Container>
         )
